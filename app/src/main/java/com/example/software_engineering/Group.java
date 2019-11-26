9291e850c605639356b1f2356e217106e2a87679
khojoooo <hojoon1619@gmail.com>
2019-11-26 22:12:33 +0900

    Group RecyclerView

--- app/src/main/java/com/example/software_engineering/Group.java
+++ app/src/main/java/com/example/software_engineering/Group.java
@@ -1,6 +1,35 @@
 package com.example.software_engineering;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+//리사이클러뷰 테스트를 위해 임시로 구현
 public class Group implements Serializable {
+    private String group_name;  //Group name
+    //private ArrayList<User> U;  //User List
+    //private ArrayList<Schedule> S;  //Schedule List
+    private int people_number;  //Number of Group people
+
+    public Group(String group_name, int people_number) {
+        this.group_name = group_name;
+        this.people_number = people_number;
+    }
+
+    public String getGroup_name() {
+        return group_name;
+    }
+
+    public void setGroup_name(String group_name) {
+        this.group_name = group_name;
+    }
+
+    public int getPeople_number() {
+        return people_number;
+    }
+
+    public void setPeople_number(int people_number) {
+        this.people_number = people_number;
+    }
+
+
 }
--- app/src/main/java/com/example/software_engineering/GroupMainActivity.java
+++ app/src/main/java/com/example/software_engineering/GroupMainActivity.java
@@ -2,12 +2,35 @@ package com.example.software_engineering;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+//리사이클러 뷰 테스트를 위해 임의 작성
 public class GroupMainActivity extends AppCompatActivity {
+
+    private ArrayList<Group> G;
+    private CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_main);
+
+        RecyclerView recyclerView = findViewById(R.id.group_view);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+
+        G = new ArrayList<>();
+        G.add(new Group("A",1));
+        G.add(new Group("B",2));
+        G.add(new Group("C",3));
+        G.add(new Group("D",4));
+
+        adapter = new CustomAdapter(G);
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
