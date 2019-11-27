e13ba353b1872c82ed4eeb0da8cdb072c8035a7f
winckey <winckey0@naver.com>
2019-11-27 20:16:11 +0900

    group sub Activity 기능 수정

--- app/src/main/java/com/example/software_engineering/GroupSubActivity.java
+++ app/src/main/java/com/example/software_engineering/GroupSubActivity.java
@@ -13,18 +13,31 @@ public class GroupSubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_sub);
     }
-    public void CreateNewGroup(String GroupName, int PeopleNumber , int GroupColor){
+    public void CreateNewGroup(String GroupName, int PeopleNumber){
         Group group = new Group(GroupName,PeopleNumber);
         User user = new User();
         user.Group_Input(group);
     }
-    /*public void ReviseGroup(String GroupName, int PeopleNumber , int GroupColor){
+    public void ReviseGroup(String GroupName, int PeopleNumber){
         User user = new User();
         ArrayList GroupLIst = user.UserGroup_Output();
-        for (Iterator<Group> iter = GroupLIst.iterator(); iter.hasNext(); ) {
-            if(iter.next().GroupName_output().equals(GroupName)){ //비교를 이름이아니고 주어진 고유 번호로 해야함
-
+        for(int i = 0; i< GroupLIst.size() ; i++){
+            if(GroupLIst.contains(GroupName)){
+                int index = GroupLIst.indexOf(GroupLIst);
+                GroupLIst.set(index,new Group(GroupName,PeopleNumber));
             }
+        }
+    }
 
-    }*/
+    public  void  DeleteGroup(String GroupName, int PeopleNumber){
+        User user = new User();
+        ArrayList GroupLIst = user.UserGroup_Output();
+        for(int i = 0; i< GroupLIst.size() ; i++){
+            if(GroupLIst.contains(GroupName)){
+                int index = GroupLIst.indexOf(GroupLIst);
+                GroupLIst.remove(index);
+            }
+        }
+    }
 }
+
