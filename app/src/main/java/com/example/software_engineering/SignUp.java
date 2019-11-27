95378f1fb38d78266aacd2a9289fd69484da007c
gkakendql <alsgh3144>
2019-11-27 14:52:24 +0900

    스케쥴 sub 없애고 스케쥴 레이아웃 구현

--- app/src/main/java/com/example/software_engineering/SignUp.java
+++ app/src/main/java/com/example/software_engineering/SignUp.java
@@ -2,11 +2,15 @@ package com.example.software_engineering;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+       EditText brand_phone = (EditText) findViewById(R.id.phone_input);
+       brand_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
     }
 }
