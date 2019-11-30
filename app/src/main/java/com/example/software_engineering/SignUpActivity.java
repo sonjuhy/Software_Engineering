e8464461fb3fe99f64ec36e8407459ad2df692a8
Red Hat <sonjuhy@gmail.com>
2019-12-01 01:22:20 +0900

    SignUpActivity

--- app/src/main/java/com/example/software_engineering/SignUpActivity.java
+++ app/src/main/java/com/example/software_engineering/SignUpActivity.java
@@ -23,18 +23,51 @@ public class SignUpActivity extends AppCompatActivity implements Serializable {
     private String PW;
     private String Phone_Num;
     private boolean result;
+    private boolean ID_Check;
 
     private Button Ok_Button;
     private Button Cancle_Button;
+    private Button IDCheck_Button;
     private EditText Name_text;
     private EditText ID_text;
     private EditText PW_text;
+    private EditText PW_Check_text;
     private EditText PhoneNum_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        IDCheck_Button = findViewById(R.id.id_check);
+        Ok_Button = findViewById(R.id.button);
+        Cancle_Button =findViewById(R.id.exit_button);
+
+        Name_text = findViewById(R.id.editText2);
+        ID_text = findViewById(R.id.id_input);
+        PW_text = findViewById(R.id.editText5);
+        PW_Check_text = findViewById(R.id.editText6);
+        PhoneNum_text = findViewById(R.id.phone_input);
+
+        System.out.println("SignUp Activity Working");
+        IDCheck_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignUp_Network signUp_network = new SignUp_Network();
+                System.out.println("ID : "+ID_text.getText().toString());
+                ID_Check = signUp_network.Network_DataArrangement("IDCheck",ID_text.getText().toString());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(!ID_Check){
+                    Toast.makeText(SignUpActivity.this, "ID Check isn't Passed", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this, "ID Check is Passed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         Ok_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,13 +90,19 @@ public class SignUpActivity extends AppCompatActivity implements Serializable {
                 }
                 else{
                     SignUp_Network signUp_network = new SignUp_Network();
-                    result = signUp_network.Network_DataArrangement("UpLoad",Name, ID, PW, Phone_Num);
-                    if(result) {
-                        Toast.makeText(SignUpActivity.this, "Sign Up is Success", Toast.LENGTH_SHORT).show();
+                    if(ID_Check) {
+                        result = signUp_network.Network_DataArrangement("UpLoad", Name, ID, PW, Phone_Num);
+                        if(result) {
+                            Toast.makeText(SignUpActivity.this, "Sign Up is Success", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this, "Sign Up is Failed", Toast.LENGTH_SHORT).show();
+                        }
                     }
                     else{
-                        Toast.makeText(SignUpActivity.this, "Sign Up is Failed", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SignUpActivity.this, "ID Check isn't Passed", Toast.LENGTH_LONG).show();
                     }
+
                 }
             }
         });
@@ -81,8 +120,9 @@ class SignUp_Network implements Serializable{
 
     private boolean result;
 
-    private void Network_Access() {
+    private void Network_Access(String Action, String Data) {
         n = new Network();//for Using Network without AsyncTask error
+        n.Input_data(Action, Data);
         try {
             Network_data = n.execute().get(); //execute Network and take return value to Network_data
         } catch (ExecutionException e) {
@@ -110,14 +150,27 @@ class SignUp_Network implements Serializable{
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
-                    n.Input_data("Schedule_UpLoad",Network_data);//Sending Data & kind of command to Network Class
-                    Network_Access();//Running Network
+                    Network_Access("Schedule_UpLoad",Network_data);//Sending Data & kind of command to Network Class Running Network
                     if(Network_data.equals(false)){ //Login Failed
                         System.out.println("UpLoad Failed");
                         return false;
                     }
                     else {//Login Success
                         System.out.println("UpLoad Success");
+                    }
+                    break;
+                case "IDCheck":
+                    try {
+                        Network_data = URLEncoder.encode("Name","UTF-8") + "=" + URLEncoder.encode(_param[1],"UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    Network_Access("SIgnUp_IDCheck",Network_data);
+                    if(Network_data.equals("Success")){
+                        result = true;
+                    }
+                    else{
+                        result = false;
                     }
                     break;
                     default:
