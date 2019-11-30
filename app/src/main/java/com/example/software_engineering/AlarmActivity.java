fdd6085b3270c218ec655de1be4b4992a4ac3918
winckey <winckey0@naver.com>
2019-11-30 19:41:55 +0900

    w저장용

---
+++ app/src/main/java/com/example/software_engineering/AlarmActivity.java
@@ -0,0 +1,58 @@
+package com.example.software_engineering;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alram);
+
+        // 알람음 재생
+       // this.mediaPlayer = MediaPlayer.create(this, R.raw.TAEYEON_Spark);/////////// 여기에 음악넣으면 됩니다!
+        this.mediaPlayer.start();
+
+        findViewById(R.id.btnClose).setOnClickListener(mClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // MediaPlayer release
+        if (this.mediaPlayer != null) {
+            this.mediaPlayer.release();
+            this.mediaPlayer = null;
+        }
+    }
+
+    /* 알람 종료 */
+    private void close() {
+        if (this.mediaPlayer.isPlaying()) {
+            this.mediaPlayer.stop();
+            this.mediaPlayer.release();
+            this.mediaPlayer = null;
+        }
+
+        finish();
+    }
+
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnClose:
+                    // 알람 종료
+                    close();
+
+                    break;
+            }
+        }
+    };
+}
